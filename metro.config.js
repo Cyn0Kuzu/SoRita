688355442183c48834b2724e0aba409cea99e56b
Cayan Kuzu <cayan@sorita.app>
2025-11-24 11:54:18 +0300

    Fix metro config for EAS build

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
 /**
  * Metro configuration
@@ -6,8 +6,6 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {
-  watchFolders: []
-};
+const config = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = config;
